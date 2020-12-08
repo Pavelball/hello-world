abbe97ad562b63331ae6d636259201621ea8acc1
Павел Балобанов <45197474+Pavelball@users.noreply.github.com>
2020-12-08 12:07:32 +0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+	    System.out.println("Hello world!");*/
+
+        /*int i = 1;
+        System.out.println(i);
+        i = i +1;
+        System.out.println(i);
+        i = i +1;
+        System.out.println(i);
+        System.out.println("Hello world!");*/
+
+        /*int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);*/
+
+        /*int a = 1;
+        int b = 2;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));*/
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + ' ' + world + '!');
+    }
+}
